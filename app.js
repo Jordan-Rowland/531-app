22fe65b2dbe099437c03cc84385c34c6298df812
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2021-08-13 20:19:16 -0700

    refactor

--- app.js
+++ app.js
@@ -5,29 +5,12 @@ document.addEventListener("alpine:init", () => {
     jokers: false,
     threeFiveOne: false,
     fivePros: false,
+    workouts: [],
+    barWeight: 45,
 
-    workouts: [
-      {
-        id: 1,
-        percentages: [.4, .5, .6],
-        reps: [5, 5, 3],
-      },
-      {
-        id: 2,
-        percentages: [.65, .75, .85, .95, 1.05],
-        reps: [5, 5, "5+", "1-3", "1-3"],
-      },
-      {
-        id: 3,
-        percentages: [.7, .8, .9, 1, 1.10],
-        reps: [3, 3, "3+", "1-3", "1-3"],
-      },
-      {
-        id: 4,
-        percentages: [.75, .85, .95, 1.05, 1.15],
-        reps: [5, 3, "1+", "1-3", "1-3"],
-      },
-    ],
+    init() {
+      this.generateMainWork();
+    },
 
     generateMainWork() {
       workouts = [
@@ -78,42 +61,52 @@ document.addEventListener("alpine:init", () => {
     },
   }));
 
-  Alpine.data("plateCalc", () => ({
-    plateCalc: false,
+  Alpine.data("workoutRow", (percentage) => ({
+    showPlates: false,
 
-    calculatePlates(total) {
-      let plate_values = [
-        45,
-        25,
-        10,
-        5,
-        2.5,
-        1.25,
-      ];
-      let plates = {};
-      const total_minus_bar = total - 45;
-      let half_total = total_minus_bar / 2;
-      for (const w of plate_values) {
-        while (half_total >= w) {
-          if (plates[w] === undefined) {
-            plates[w] = 1;
-          } else {
-            plates[w] += 1;
-          }
-          half_total -= w;
-        }
-      }
-      let formatted_plates = [];
-      for (
-        const [key, value] of Object
-          .entries(plates)
-          .sort(
-            (a, b) => b[0] - a[0],
-          )
-      ) {
-        formatted_plates.push(`<b>${key}</b>: ${value}`);
-      }
-      return formatted_plates.join(" | ");
+    percentageCalc() {
+      return Math.round(percentage * 100);
     },
+
+    percentageAndWeight() {
+      return `${this.percentageCalc(percentage)}% - ${this.roundToWeight(this.tm * percentage)}lbs`
+    },
+
   }));
 });
+
+
+function calculatePlates(total, barWeight=45) {
+  let plate_values = [
+    45,
+    25,
+    10,
+    5,
+    2.5,
+    1.25,
+  ];
+  let plates = {};
+  const total_minus_bar = total - barWeight;
+  let half_total = total_minus_bar / 2;
+  for (const w of plate_values) {
+    while (half_total >= w) {
+      if (plates[w] === undefined) {
+        plates[w] = 1;
+      } else {
+        plates[w] += 1;
+      }
+      half_total -= w;
+    }
+  }
+  let formatted_plates = [];
+  for (
+    const [key, value] of Object
+      .entries(plates)
+      .sort(
+        (a, b) => b[0] - a[0],
+      )
+  ) {
+    formatted_plates.push(`<b>${key}</b>: ${value}`);
+  }
+  return formatted_plates.join(" | ");
+}
